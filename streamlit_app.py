f2bdfdbd709d2bb949dbe84e1c0810a70275642a
kirubhaharini <kirubhah@gmail.com>
2021-07-13 21:09:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,11 @@ db = firestore.Client.from_service_account_json("firestore-key.json")
 doc_id = db.collection("users")
 
 docs = doc_id.stream()
-
+# print(docs)
+# for doc in docs:
+#     print(doc.to_dict())
 my_dict = { doc.id: doc.to_dict() for doc in docs }
+#print(my_dict)
 df = pd.DataFrame.from_dict(my_dict)
 df = df.transpose()
 placeholder = st.empty()
@@ -127,5 +130,88 @@ if followers:
        
         
     placeholder.table(filtered_df)
+
+
+#engagement rate - comments
+comments_df = pd.DataFrame()
+comments = st.sidebar.slider(
+    'Select engagement rate for comments',
+   0,100, (25,75), step=5
+)
+min_fol = comments[0]
+max_fol = comments[1]
+col = df.columns.get_loc("engagement_comments")
+#print(col)
+
+if comments:
+    for row in range((df.shape)[0]): 
+        rate = df.iat[row,col]
+        # print(min_fol)
+        # print(max_fol)
+        if rate == 'NA':
+            rate = 0
+        if min_fol <= float(rate) <= max_fol:
+            #apply filter
+            comments_df = comments_df.append(df.iloc[row,:])
+
+    if comments_df.empty == True:
+        filtered_df = pd.DataFrame()
+        for col in comments_df: 
+            comments_df[col] = comments_df[col].fillna('NA')
+    if (filtered_df.empty == False and comments_df.empty == False):
+        print(comments_df)
+        print(filtered_df)
+        filtered_df["engagement_comments"] = filtered_df["engagement_comments"].astype(str)
+        comments_df["engagement_comments"] = comments_df["engagement_comments"].astype(str)
+
+        filtered_df = pd.merge(comments_df,filtered_df, how = 'inner')
+        print(filtered_df)
+
+       
+        
+    placeholder.table(filtered_df)
+
+
+
+#engagement rate - likes
+likes_df = pd.DataFrame()
+likes = st.sidebar.slider(
+    'Select engagement rate for likes',
+   0,100, (25,75), step=5
+)
+min_fol = likes[0]
+max_fol = likes[1]
+col = df.columns.get_loc("engagement_likes")
+#print(col)
+
+if likes:
+    for row in range((df.shape)[0]): 
+        rate = df.iat[row,col]
+        # print(min_fol)
+        # print(max_fol)
+        if rate == 'NA':
+            rate = 0
+        if min_fol <= float(rate) <= max_fol:
+            #apply filter
+            likes_df = likes_df.append(df.iloc[row,:])
+
+    if likes_df.empty == True:
+        filtered_df = pd.DataFrame()
+        for col in likes_df: 
+            likes_df[col] = likes_df[col].fillna('NA')
+    if (filtered_df.empty == False and likes_df.empty == False):
+        print(likes_df)
+        print(filtered_df)
+        filtered_df["engagement_likes"] = filtered_df["engagement_likes"].astype(str)
+        likes_df["engagement_likes"] = likes_df["engagement_likes"].astype(str)
+
+        filtered_df = pd.merge(likes_df,filtered_df, how = 'inner')
+        print(filtered_df)
+
+       
+        
+    placeholder.table(filtered_df)
+
+
     
 placeholder.table(filtered_df)
