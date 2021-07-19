e3146ad1f896ffffa7c7305bcdd0b9d5b3bc1fe4
kirubhaharini <59768501+kirubhaharini@users.noreply.github.com>
2021-07-20 00:12:48 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -17,21 +17,3 @@ s = a
 print(s)
 def returnUsername():
     return s
-# t = mod.returnRes()
-# st.write(t)
-
-# def temp1():
-#         return 8
-
-# result = mod.pyt()
-# print('final: ',result)
-
-
-# st.title('Home')
-
-# st.write('This is the `home page` of this multi-page app.')
-
-# st.write('In this app, we will be building a simple classification model using the Iris dataset.')
-# hashtag_filter = st.sidebar.multiselect(
-# 'Select hashtag',
-# options=['1','3'])
