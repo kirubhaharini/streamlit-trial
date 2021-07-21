b094f7cc9d4d0e406c9d6165673ab366e6e7637f
kirubhaharini <59768501+kirubhaharini@users.noreply.github.com>
2021-07-21 11:40:33 +0800

    Update home.py

--- home.py
+++ home.py
@@ -25,9 +25,9 @@ default_title = app_state["username"] if "username" in app_state else ""
 title = st.text_input('Influencer Username', value = default_title)
 app_state["username"] = title
 
-st.write(st.experimental_get_query_params())
+st.write(st.experimental_get_query_params()["username"])
 
-a = st.experimental_get_query_params()["username"][0]
+a = (st.experimental_get_query_params())["username"][0]
 
 
 # Authenticate to Firestore with the JSON account key.
