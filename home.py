548336898d7eabfac5a667fa7bb9ca46060bc30b
kirubhaharini <59768501+kirubhaharini@users.noreply.github.com>
2021-07-21 11:41:09 +0800

    Update home.py

--- home.py
+++ home.py
@@ -25,7 +25,7 @@ default_title = app_state["username"] if "username" in app_state else ""
 title = st.text_input('Influencer Username', value = default_title)
 app_state["username"] = title
 
-st.write(st.experimental_get_query_params()["username"])
+st.write(st.experimental_get_query_params()["username"][0])
 
 a = (st.experimental_get_query_params())["username"][0]
 
